a35be670ee66bc96e5d41a6b63bcb061d975dcb9
01stone01 <hagwhr2@gmail.com>
2025-03-23 18:06:52 +0900

    [fix] resolves #2
    #2 버그 내용
    판매 시 출력이 이상하게 되는 현상
    
    해결 : 구문 순서 변경

--- Store_Game/Store_Game/Program.cs
+++ Store_Game/Store_Game/Program.cs
@@ -2,27 +2,6 @@
 {
     internal class Program
     {
-        /*1. 상점에서는 다음 작업들이 가능하다.
-            A. 아이템 구매
-            B. 아이템 판매
-            C. 아이템 확인
-
-          2. 아이템은 기본적으로 이름, 설명, 가격을 가지고 있으며, 
-             무기는 공격력, 방어구는 방어력, 장신구는 체력을 상승시키는 수치를 가진다.
-
-          3. 아이템 구매 메뉴 선택시 상점이 소유하고 있는 아이템들 목록이 제공되고, 
-             구매하고자 하는 아이템을 선택시 구매를 진행한다. 
-             단, 돈이 부족하다면 구매는 진행되지 않는다.
-             구매가 완료되면 소유한 아이템에 구매한 아이템이 추가되며, 
-             아이템에 의해 플레이어 능력이 상승한다.
-          4. 아이템 판매 메뉴 선택시 플레이어가 소유하고 있는 아이템들 목록이 제공되고, 
-             판매하고자 하는 아이템을 선택시 판매를 진행한다. 
-             단, 소유한 아이템이 없다면 진행되지 않는다.
-             판매가 완료되면 소유한 아이템에 판매한 아이템이 제거되며, 
-             아이템에 의해 플레이어 능력이 하락한다.
-          5. 아이템 확인 메뉴 선택시 플레이어가 소유하고 있는 아이템들 목록이 제공되고, 
-             아이템들에 의해 상승한 플레이어 최종 능력치를 보여준다.
-             플레이어는 최대 6개의 아이템을 소유할 수 있으며 빈칸은 보여주지 않는다.*/
         #region struct
         struct Item
         {
@@ -82,7 +61,7 @@
         #region Render
         static void Render(ref int gold, Item longSword, Item clothArmor, Item tearOfGoddess, ref int attack, ref int defense, ref int health, ref List<Item> inventory)
         {
-            MainMenu(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);     // 상점 메인 메뉴            
+            MainMenu(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);        
         }
         #endregion
         #region MainMenu
@@ -179,18 +158,8 @@
             {
                 Console.Write("보유하신 아이템이 없습니다.\n\n계속하려면 아무키나 입력하세요 : ");
             }
-
+            
             ConfigMenuInput(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
-
-            //=======아이템 확인=======
-            //플레이어 골드 보유량 : {}G
-            //플레이어 공격력 상승량 : {}
-            //플레이어 방어력 상승량 : {}
-            //플레이어 체력 상승량 : {}
-            //1. 여신의 눈물
-            //2. 롱소드
-            //인벤토리 기능
-            //계속하려면 아무키나 입력하세요 : 
         }
         #endregion
         #region MainInput
@@ -223,7 +192,7 @@
         {
             Console.Write("구매할 아이템을 선택하세요 (취소 0) : ");
             ConsoleKey key = Console.ReadKey().Key;
-            Console.WriteLine("");
+            Console.WriteLine("\n");
 
             switch (key)
             {
@@ -299,14 +268,14 @@
                     }
                     break;
                 case ConsoleKey.D3:
-                    Console.WriteLine($"{tearOfGoddess.name}를 구매합니다.");
-                    Console.WriteLine($"플레이어의 {tearOfGoddess.enfo}이 {tearOfGoddess.value} 상승하여 {health += tearOfGoddess.value}이 됩니다.");
-                    Console.WriteLine($"보유한 골드가 {tearOfGoddess.price} 감소하여 {gold -= tearOfGoddess.price}G가 됩니다.\n");
-                    inventory.Add(tearOfGoddess);
-                    Console.Write("계속하려면 아무키나 입력하세요 (돌아가기 0) : ");
-                    ConsoleKey key5 = Console.ReadKey().Key;
                     if (inventory.Count < 5)
                     {
+                        Console.WriteLine($"{tearOfGoddess.name}를 구매합니다.");
+                        Console.WriteLine($"플레이어의 {tearOfGoddess.enfo}이 {tearOfGoddess.value} 상승하여 {health += tearOfGoddess.value}이 됩니다.");
+                        Console.WriteLine($"보유한 골드가 {tearOfGoddess.price} 감소하여 {gold -= tearOfGoddess.price}G가 됩니다.\n");
+                        inventory.Add(tearOfGoddess);
+                        Console.Write("계속하려면 아무키나 입력하세요 (돌아가기 0) : ");
+                        ConsoleKey key5 = Console.ReadKey().Key;
                         switch (key5)
                         {
                             case ConsoleKey.D0:
@@ -340,8 +309,6 @@
                     ItemBuyMenu(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                     break;
             }
-
-
         }
         #endregion
         #region SellMenuInput
@@ -352,6 +319,7 @@
             Console.WriteLine("");
 
             if (int.Parse(key) > 0 && int.Parse(key) <= inventory.Count)
+            {
                 for (int i = 1; i <= inventory.Count; i++)
                 {
                     if (int.Parse(key) == i)
@@ -370,7 +338,7 @@
                                     Render(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                                     break;
                                 default:
-                                    SellMenuInput(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
+                                    ItemSellMenu(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                                     break;
                             }
                         }
@@ -388,7 +356,7 @@
                                     Render(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                                     break;
                                 default:
-                                    SellMenuInput(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
+                                    ItemSellMenu(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                                     break;
                             }
                         }
@@ -406,12 +374,13 @@
                                     Render(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                                     break;
                                 default:
-                                    SellMenuInput(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
+                                    ItemSellMenu(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                                     break;
                             }
                         }
                     }
                 }
+            }
             else if (key == "0")
             {
                 Render(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
@@ -436,36 +405,6 @@
                     Render(ref gold, longSword, clothArmor, tearOfGoddess, ref attack, ref defense, ref health, ref inventory);
                     break;
             }
-        }
-        #endregion
-        #region Update
-        static void Update()
-        {
-            Gold();
-            Value();
-        }
-        #endregion
-        #region Gold
-        static void Gold()
-        {
-            //아이템 샀을 때 감소
-            //아이템 팔 때 감소
-            //골드 보유량 ref 타입
-        }
-        #endregion
-        #region Value
-        static void Value()
-        {
-            //아이템에 따른 수치 증가량
-            //attack
-            //defense
-            //Health
-        }
-        #endregion
-        #region End
-        static void End()
-        {
-
         }
         #endregion
     }
